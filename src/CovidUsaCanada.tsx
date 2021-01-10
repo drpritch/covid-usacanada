6194103cefab15fa13771b12d05fe4d7170b9fb7
David Pritchard <davidp@mentalcanvas.com>
2021-01-09 21:24:11 -0500

    React app - add a slider to choose a week.

--- src/CovidUsaCanada.tsx
+++ src/CovidUsaCanada.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DeckGL from '@deck.gl/react';
 import {GeoJsonLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
+import Slider from '@material-ui/core/slider';
 
 const COLOR_SCALE = [
     // from R: scales::brewer_pal(palette='GnBu')(9)
@@ -11,52 +12,77 @@ const COLOR_SCALE = [
 ];
 const BREAKS = [ 25, 100, 200, 300, 400, 700, 1000, 1300, 100000 ];
 
-function CovidUsaCanada({props, state}) {
-    const layer = new GeoJsonLayer({
-        id: 'geojson-layer',
-        data: 'covidUsaCanada.json',
-        pickable: true,
-        stroked: true,
-        filled: true,
-        extruded: false,
-        lineWidthScale: 20,
-        lineWidthMinPixels: 0.5,
-        getFillColor: f => {
-            const hex = COLOR_SCALE[BREAKS.findIndex(x => x > f.properties.cases53 / f.properties.pop100k)];
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            return result ? [
-                parseInt(result[1], 16),
-                parseInt(result[2], 16),
-                parseInt(result[3], 16)
-            ] : null;
-        },
-        opacity: 0.7,
-        getLineColor: [80,80,80,40],
-        Radius: 100,
-        getLineWidth: 1,
-        getElevation: 0
-    });
+type CUCState = {
+    week: number;
+}
 
-    return <DeckGL viewState={state}
-        layers={[layer]}
-        mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-        initialViewState={{
-            longitude: -122.4,
-            latitude: 37.74,
-            zoom: 11,
-            maxZoom: 20,
-            pitch: 0,
-            bearing: 0
-        }}
-        controller={true}
-        getTooltip={({object}) => {
-            if (!object) return null;
-            const p = object.properties;
-            return p.health_region + '\n' +
-                Math.round(p.cases53 / p.pop100k) + ' weekly cases/100k' + '\n' +
-                Math.round(p.pop100k * 100000) + ' people';
-    }}>
-        <StaticMap mapStyle='mapbox://styles/mapbox/light-v10'/>
-    </DeckGL>;
+export class CovidUsaCanada extends React.Component<{}, CUCState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            week: 53
+        };
+    }
+    render() {
+        const getFillColor = (feature, week) => {
+                const hex = COLOR_SCALE[
+                        BREAKS.findIndex(x => x > feature.properties['cases' + week] / feature.properties.pop100k)
+                ];
+                var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+                return result ? [
+                        parseInt(result[1], 16),
+                        parseInt(result[2], 16),
+                        parseInt(result[3], 16)
+                ] : null;
+            };
+        
+        const layer = new GeoJsonLayer({
+                id: 'geojson-layer',
+                data: 'covidUsaCanada.json',
+                pickable: true,
+                stroked: true,
+                filled: true,
+                extruded: false,
+                lineWidthScale: 20,
+                lineWidthMinPixels: 0.5,
+                getFillColor: (f) => getFillColor(f, this.state.week),
+                updateTriggers: {
+                        getFillColor: this.state.week
+                },
+                opacity: 0.7,
+                getLineColor: [80,80,80,40],
+                Radius: 100,
+                getLineWidth: 1,
+                getElevation: 0
+        });
+
+        return <div>
+                <DeckGL
+                        layers={[layer]}
+                        mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+                        initialViewState={{
+                                longitude: -79.350,
+                                latitude: 44.382,
+                                zoom: 6,
+                                maxZoom: 11,
+                                pitch: 0,
+                                bearing: 0
+                        }}
+                        controller={true}
+                        getTooltip={({object}) => {
+                                if (!object) return null;
+                                const p = object.properties;
+                                return p.health_region + '\n' +
+                                        Math.round(p['cases' + this.state.week] / p.pop100k) + ' weekly cases/100k' + '\n' +
+                                        Math.round(p.pop100k * 100000) + ' people';
+                        }}>
+                    {/*<StaticMap mapStyle='mapbox://styles/mapbox/light-v10'/>*/}
+                </DeckGL>
+                <div>
+                        <Slider defaultValue={53} min={12} max={53}
+                                onChange={(event: ReactChangeEvent<{}>, newValue: number) => this.setState({week: newValue})}></Slider>
+                </div>
+        </div>;
+    }
 }
 export default CovidUsaCanada;
